69ca8fa720cdc61267fc01f0aa3567b68de37660
Gabriel Lima <email.gabriel.lima@gmail.com>
2018-06-07 20:34:53 +0100

    service-api: add local environment variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  api_username = environment.api_username;
+  api_secret = environment.api_secret;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,10 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+import { environment_config } from 'environments/environment.config';
+
 export const environment = {
-  production: false
+  production: false,
+  api_username: environment_config.PAYVISION_USERNAME || null,
+  api_secret: environment_config.PAYVISION_SECRET || null
 };
