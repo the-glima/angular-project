be6a13a06825ec127634c382468b1fed37f1cde2
Gabriel Lima <email.gabriel.lima@gmail.com>
2019-04-09 00:07:07 +0200

    Add ng-svg-icon-sprite

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IconSpriteModule } from 'ng-svg-icon-sprite';
+
 import * as fromCommonComponents from '@common/components';
 import * as fromServices from '@common/services';
 import * as fromTransactionsServices from '@transactions/services';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    IconSpriteModule,
+    IconSpriteModule.forRoot({ path: 'assets/icons/sprite.svg' })
+  ],
   declarations: [...Object.values(fromCommonComponents)],
   exports: [...Object.values(fromCommonComponents)],
   providers: [
