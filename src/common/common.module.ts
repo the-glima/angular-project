708c321ca0dbe308e722a0216e137234f804ac31
Gabriel Lima <email.gabriel.lima@gmail.com>
2019-04-14 16:56:20 +0200

    Temporary change AOT build

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
-
 import * as fromCommonComponents from '@common/components';
 
 @NgModule({
